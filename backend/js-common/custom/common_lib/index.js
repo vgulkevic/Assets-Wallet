c3b7e6366df7b16aae920bd89c34c3724e4428f4
vgulkevic <vadims.gulkevics@yahoo.com>
2020-06-15 21:55:38 +0100

    Clean up

--- backend/js-common/custom/common_lib/index.js
+++ backend/js-common/custom/common_lib/index.js
@@ -1,26 +1,11 @@
 const {ParameterStore} = require('./parameterStore.js');
 const {DynamoDbClient} = require('./dynamoDbClient.js');
-const {QboClient, QboClientFactory} = require('./qboClient.js');
 const {EventPublisher} = require('./eventPublisher.js');
 const {ApiGatewayUtils} = require('./apiGatewayUtils.js');
-const {SlackUtils} = require('./slackUtils.js');
-
-const {QboCustomersOperations} = require(
-    './qbo_persistence/QboCustomersOperations.js');
-const {QboCredentialsOperations} = require(
-    './qbo_persistence/QboCredentialsOperations.js');
-const {QboSalesItemsOperations} = require(
-    './qbo_persistence/QboSalesItemsOperations.js');
 
 module.exports = {
   ParameterStore,
   DynamoDbClient,
-  QboClient,
-  QboClientFactory,
-  QboCustomersOperations,
-  QboCredentialsOperations,
-  QboSalesItemsOperations,
   EventPublisher,
-  ApiGatewayUtils,
-  SlackUtils
+  ApiGatewayUtils
 }
